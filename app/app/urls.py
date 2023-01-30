c36a0053ca8c97362f23503d5b5aa3e5ab6bf80e
KarinaKhazanova <karinakhazanova@gmail.com>
2023-01-30 11:21:55 +0200

    newchanges

--- app/app/urls.py
+++ app/app/urls.py
@@ -32,7 +32,4 @@ urlpatterns = [
         "profi/",
         include("django.contrib.auth.urls"),
     ),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
